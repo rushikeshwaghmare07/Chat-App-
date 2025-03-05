d6d7c99e4276ad5bda27bb56eb9cf22bf6ff26f3
Rushikesh Ramesh Waghmare <rushikeshwaghmare862@gmail.com>
2025-03-06 00:54:17 +0530

    Add validation for user schema

---
+++ src/user.validation.js
@@ -0,0 +1,11 @@
+import { z } from "zod";
+
+export const signupSchema = z.object({
+  fullName: z
+    .string()
+    .min(2, { message: "Full name must be at least 2 characters long" }),
+  email: z.string().email({ message: "Invalid email address" }),
+  password: z
+    .string()
+    .min(6, { message: "Password must be at least 6 characters long" }),
+});
