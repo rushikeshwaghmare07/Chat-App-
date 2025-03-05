44f48c18ed936392c3f20d76cb3c87a178871c68
Rushikesh Ramesh Waghmare <rushikeshwaghmare862@gmail.com>
2025-03-05 20:39:56 +0530

    Add MongoDB connection setup

--- src/index.js
+++ src/index.js
@@ -1,10 +1,18 @@
 import dotenv from "dotenv";
 import express from "express";
 
+import { connectDB } from "./db/index.js";
+
 dotenv.config();
 const app = express();
 const PORT = process.env.PORT || 3001;
 
-app.listen(PORT, () => {
-  console.log("Server is running on port:", PORT);
-});
+connectDB()
+  .then(() => {
+    app.listen(PORT, () => {
+      console.log("Server is running on port:", PORT);
+    });
+  })
+  .catch((error) => {
+    console.log("MongoDB connection error:", error);
+  });
