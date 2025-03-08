11de2badcb8cc70a57c4ce33aca10229962080a6
Rushikesh Ramesh Waghmare <rushikeshwaghmare862@gmail.com>
2025-03-08 14:51:36 +0530

    Add auth middleware

---
+++ src/middleware/auth.middleware.js
@@ -0,0 +1,41 @@
+import jwt from "jsonwebtoken";
+import User from "../models/user.model";
+
+export const protectRoutes = async (req, res, next) => {
+  try {
+    const token = req.cookies.jwt || req.header.Authorization.split(" ")[1];
+    if (!token) {
+      return res.status(401).json({
+        success: false,
+        message: "Unauthorized - No Token Provided",
+      });
+    }
+
+    const decodedToken = jwt.verify(token, process.env.JWT_SECRET);
+    if (!decodedToken) {
+      return res.status(401).json({
+        success: false,
+        message: "Not authorized to access this route",
+      });
+    }
+
+    const user = await User.findById(decodedToken.userId);
+    if (!user) {
+      return res.status(404).json({
+        success: false,
+        message: "User not found",
+      });
+    }
+
+    req.user = user;
+
+    next();
+  } catch (error) {
+    console.log("Error in protectRoutes middleware:", error);
+    return res.status(500).json({
+      success: false,
+      message: "Internal server error",
+      error: error.message,
+    });
+  }
+};
