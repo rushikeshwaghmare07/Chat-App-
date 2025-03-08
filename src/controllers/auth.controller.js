8ffcce2e1638ecec6f597ba6763dc01624f329a8
Rushikesh Ramesh Waghmare <rushikeshwaghmare862@gmail.com>
2025-03-08 15:03:19 +0530

    Add update profile functionality

--- src/controllers/auth.controller.js
+++ src/controllers/auth.controller.js
@@ -1,6 +1,7 @@
 import User from "../models/user.model.js";
 import { signupSchema } from "../validations/user.validation.js";
 import { generateToken } from "../utils/jwt.js";
+import cloudinary from "../utils/cloudinary.js";
 
 export const signup = async (req, res) => {
   try {
@@ -114,6 +115,40 @@ export const logout = async (req, res) => {
     });
   } catch (error) {
     console.log("Error in logout controller:", error);
+    return res.status(500).json({
+      success: false,
+      message: "Internal server error",
+      error: error.message,
+    });
+  }
+};
+
+export const updateProfile = async (req, res) => {
+  try {
+    const userId = req.user._id;
+    const { profilePic } = req.body;
+
+    if (!profilePic) {
+      return res.status(400).json({
+        success: false,
+        message: "Profile pic is required.",
+      });
+    }
+
+    const uploadResponse = await cloudinary.uploader.upload(profilePic);
+    const updateUser = await User.findByIdAndUpdate(
+      userId,
+      { profilePic: uploadResponse.secure_url },
+      { new: true }
+    );
+
+    return res.status(200).json({
+      success: true,
+      message: "Profile updated successfully",
+      updateUser,
+    });
+  } catch (error) {
+    console.log("Error in updateProfile controller:", error);
     return res.status(500).json({
       success: false,
       message: "Internal server error",
--- src/routes/auth.routes.js
+++ src/routes/auth.routes.js
@@ -1,10 +1,13 @@
 import express from "express";
-import { signin, signup, logout } from "../controllers/auth.controller.js";
+import { signin, signup, logout, updateProfile } from "../controllers/auth.controller.js";
+import { protectRoutes } from "../middleware/auth.middleware.js";
 
 const router = express.Router();
 
 router.post("/signup", signup);
 router.post("/signin", signin);
 router.post("/signout", logout);
+
+router.put("/updateProfile", protectRoutes, updateProfile);
 
 export default router;
