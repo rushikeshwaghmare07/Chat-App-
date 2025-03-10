d1f8c03d4910e4252ba0910b624d2c34250e5818
Rushikesh Ramesh Waghmare <rushikeshwaghmare862@gmail.com>
2025-03-10 22:29:18 +0530

    Add routing and pages

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,10 +1,25 @@
-function App() {
+import { Route, Routes } from "react-router-dom";
+import Navbar from "./components/Navbar";
+import HomePage from "./pages/HomePage";
+import SignupPage from "./pages/SignupPage";
+import LoginPage from "./pages/LoginPage";
+import SettingPage from "./pages/SettingPage";
+import ProfilePage from "./pages/ProfilePage";
 
+function App() {
   return (
     <>
-      <h1>Chat Application</h1>
+      <Navbar />
+
+      <Routes>
+        <Route path="/" element={<HomePage />} />
+        <Route path="/signup" element={<SignupPage />} />
+        <Route path="/login" element={<LoginPage />} />
+        <Route path="/settings" element={<SettingPage />} />
+        <Route path="/profile" element={<ProfilePage />} />
+      </Routes>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
