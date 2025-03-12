a2a40aac48d676d822a4b98edd04e32a6a0a7e3f
Rushikesh Ramesh Waghmare <rushikeshwaghmare862@gmail.com>
2025-03-12 21:10:05 +0530

    Add logout function

--- frontend/src/store/useAuthStore.js
+++ frontend/src/store/useAuthStore.js
@@ -37,4 +37,14 @@ export const useAuthStore = create((set) => ({
       set({ isSignup: false });
     }
   },
+
+  logout: async () => {
+    try {
+      await axiosInstance.post("/auth/logout");
+      set({ authUser: null });
+      toast.success("Logged out successfully");
+    } catch (error) {
+      toast.error(error.response.data.message);
+    }
+  }
 }));
